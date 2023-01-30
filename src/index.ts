66d9692855a4650a4abc1b7eaea6f224c2a5f162
Jay <jay.bienaime@gmail.com>
2023-01-30 19:10:16 +0000

    canvas setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-alert("hello world")
+const appDiv: HTMLElement = document.getElementById('app');
+const canvas: HTMLCanvasElement = document.querySelector('canvas');
+const context = canvas.getContext('2d');
+const defaultSettings = {
+  canvasWidth: 1024,
+  canvasHeight: 576,
+};
+const { canvasWidth, canvasHeight } = defaultSettings;
+
+canvas.width = canvasWidth;
+canvas.height = canvasHeight;
+context.fillRect(0, 0, canvasWidth, canvasHeight);
